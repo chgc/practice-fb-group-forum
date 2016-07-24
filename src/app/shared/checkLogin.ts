a6a113193bf52be9a2d43b8a0601f91e4af2aec7
Kevin Yang <Kevin Yang>
2016-07-24 21:46:02 +0800

    modify checkLogin to do login action

--- src/app/shared/checkLogin.ts
+++ src/app/shared/checkLogin.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Router, CanActivate} from '@angular/router';
-
+import { Router, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 import { FbService } from '../post/shared/index'
 
 @Injectable()
@@ -8,10 +8,15 @@ export class CheckLogin implements CanActivate {
     constructor(private router: Router, private fb: FbService) { }
 
     canActivate() {
-        if (this.fb.userObj !== undefined){
-            return true;
-        }
-        this.router.navigate(['/posts']);
-        return false;
+        return Observable.fromPromise(this.fb.login())
+                   .map(res=>{                     
+                      return true;
+                   });
+
+        // if (this.fb.userObj !== undefined){
+        //     return true;
+        // }
+        // this.router.navigate(['/posts']);
+        // return false;
     }
 }
